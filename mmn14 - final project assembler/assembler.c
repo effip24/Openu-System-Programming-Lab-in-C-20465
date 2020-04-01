d3868b7a7f1585877d22381124f467c74e44494e
Ephraim Phil <51086925+effip24@users.noreply.github.com>
2020-04-02 01:41:30 +0300

    Update assembler.c

--- mmn14 - final project assembler/assembler.c
+++ mmn14 - final project assembler/assembler.c
@@ -2,7 +2,7 @@
 mmn14 - two pass assembler model based on imaginary computer
 and imaginary assembly language.
 assembler.c - this file has the main function.
-@author - Ephraim Phil 205390156.
+@author - Ephraim Phil.
 @version - 10/03/2020. */
 
 #include <stdio.h>
